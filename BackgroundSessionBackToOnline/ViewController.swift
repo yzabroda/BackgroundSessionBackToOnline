f35ef1f3027505b7b519015cc4784ac41c579e4b
Yuriy Zabroda <Yuriy.Zabroda@ua.ibm.com>
2019-05-28 13:37:32 +0300

    Initial Commit

---
+++ BackgroundSessionBackToOnline/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BackgroundSessionBackToOnline
+//
+//  Created by Yuriy Zabroda on 5/28/19.
+//  Copyright © 2019 Yuriy Zabroda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
