83301685df9ed319aa32698eed00e6a2e9163fc5
Yuriy Zabroda <Yuriy.Zabroda@ua.ibm.com>
2019-06-11 18:28:18 +0300

    Almost finished

--- BackgroundSessionBackToOnline/AppDelegate.swift
+++ BackgroundSessionBackToOnline/AppDelegate.swift
@@ -6,7 +6,10 @@
 //  Copyright © 2019 Yuriy Zabroda. All rights reserved.
 //
 
+import os.log
 import UIKit
+
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,19 +18,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        os_log("------ didFinishLaunchingWithOptions")
+        let _ = BigFileDownloadManager.shared.urlSession
+
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        os_log("------ applicationWillResignActive")
     }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+
+    func application(_ application: UIApplication, handleEventsForBackgroundURLSession identifier: String, completionHandler: @escaping () -> Void) {
+        guard identifier == "com.bomzh.uu" else { return }
+
+        os_log("------ handleEventsForBackgroundURLSession; state: %{public}d", application.applicationState.rawValue)
+        os_log("------ handleEventsForBackgroundURLSession: backgroundTimeRemaining = %{public}f", application.backgroundTimeRemaining)
+
+        backgroundCompletionHandler = completionHandler
     }
+
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        backgroundTaskIdentifier = application.beginBackgroundTask { [weak self] in
+            guard let strongSelf = self else { return }
+
+            application.endBackgroundTask(strongSelf.backgroundTaskIdentifier)
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
+            os_log("------ Going to crash now!!!!!!!")
+            exit(EXIT_SUCCESS)
+        }
+    }
+
+    var backgroundTaskIdentifier = UIBackgroundTaskIdentifier.invalid
+    
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
@@ -35,6 +63,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        os_log("------ applicationDidBecomeActive")
+        BigFileDownloadManager.shared.forceResume()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
@@ -42,5 +72,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+    private(set) var backgroundCompletionHandler: (() -> Void)?
 }
 
--- BackgroundSessionBackToOnline/ViewController.swift
+++ BackgroundSessionBackToOnline/ViewController.swift
@@ -16,14 +16,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        overallProgress = Progress.discreteProgress(totalUnitCount: Int64(filesToDownload.count))
     }
 
 
     @IBAction func resumeDownload(_ sender: UIButton) {
-        theProgressView.observedProgress = overallProgress
+        theProgressView.observedProgress = BigFileDownloadManager.shared.launchDownload()
 
-        fractionCompletedObservation = overallProgress.observe(\.fractionCompleted) { [weak self] progress, _ in
+        fractionCompletedObservation = theProgressView.observedProgress!.observe(\.fractionCompleted) { [weak self] progress, _ in
             guard let strongSelf = self else { return }
 
             OperationQueue.main.addOperation {
@@ -31,13 +30,6 @@ class ViewController: UIViewController {
                 strongSelf.additionalDescriptionLabel.text = progress.localizedAdditionalDescription
             }
         }
-
-        let url = URL(string: "https://devstreaming-cdn.apple.com/videos/wwdc/2017/250lnw83hnjfutowrg/250/250_hd_extend_your_apps_presence_with_deep_linking.mp4?dl=1")!
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 3.0)
-
-        let downloadTask = urlSession.downloadTask(with: request)
-        overallProgress.addChild(downloadTask.progress, withPendingUnitCount: 1)
-        downloadTask.resume()
     }
 
 
@@ -46,72 +38,188 @@ class ViewController: UIViewController {
     @IBOutlet var additionalDescriptionLabel: UILabel!
     
 
-    private final func startDownload() {
-        guard currentFileIndex < filesToDownload.count else {
-            os_log("--- Downloading finished!!!")
-
-            return
-        }
-
-        let url = URL(string: filesToDownload[currentFileIndex])!
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 3.0)
-
-        let downloadTask = urlSession.downloadTask(with: request)
-        overallProgress.addChild(downloadTask.progress, withPendingUnitCount: 1)
-        downloadTask.resume()
-    }
-
-
-    private lazy var urlSession: URLSession = {
-        let config = URLSessionConfiguration.background(withIdentifier: "com.bomzh.uu")
-
-        return URLSession(configuration: config, delegate: self, delegateQueue: nil)
-    }()
-
-
-    var downloadTaskAddresses = Set<String>()
-
-    private let filesToDownload = [
-//        "https://devstreaming-cdn.apple.com/videos/wwdc/2017/250lnw83hnjfutowrg/250/250_sd_extend_your_apps_presence_with_deep_linking.mp4?dl=1",
-        "https://devstreaming-cdn.apple.com/videos/tutorials/ensuring_beautiful_rich_links/ensuring_beautiful_rich_links_sd.mp4?dl=1",
-        "https://devstreaming-cdn.apple.com/videos/tutorials/web_inspector_walkthrough/web_inspector_walkthrough_sd.mp4?dl=1",
-        "https://devstreaming-cdn.apple.com/videos/tutorials/using_web_inspector_with_tvos_apps/using_web_inspector_with_tvos_apps_sd.mp4?dl=1" // ,
-//        "https://devstreaming-cdn.apple.com/videos/tutorials/20170912/202uhvrcg65c7/updating_your_app_for_apple_tv_4k/updating_your_app_for_apple_tv_4k_sd.mp4?dl=1"
-    ]
-
-    private var overallProgress: Progress!
-    private var currentFileIndex = 0
     private var fractionCompletedObservation: NSKeyValueObservation?
 }
 
 
+class BigFileDownloadManager: NSObject {
+    
+    static let shared = BigFileDownloadManager()
+    
+    static let backgroundSessionIdentifier = "com.weather.pilotbrief.download-manager"
 
-extension ViewController: URLSessionDownloadDelegate {
+    final func launchDownload() -> Progress? {
+        let overProg = Progress.discreteProgress(totalUnitCount: Int64(filesToDownload.count))
+        overallProgress = overProg
+        startDownload()
 
-    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
-        if let error = error {
-            os_log("--- didCompleteWithError: %{public}@", error.localizedDescription)
-        } else {
-            os_log("--- didCompleteWithError: nil")
-            DispatchQueue.main.async { [weak self] in
-                guard let strongSelf = self else { return }
+        return overProg
+    }
 
-                strongSelf.currentFileIndex += 1
-                strongSelf.startDownload()
+
+    final func startDownload() {
+        for fileToDown in filesToDownload {
+            let url = URL(string: fileToDown)!
+            let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 3.0)
+
+            os_log("------ Going to create a downloadTask")
+            let downloadTask = urlSession.downloadTask(with: request)
+            overallProgress!.addChild(downloadTask.progress, withPendingUnitCount: 1)
+        }
+
+        urlSession.getTasksWithCompletionHandler { _, _, downloadTasks in
+            DispatchQueue.main.async {
+                downloadTasks.first(where: { $0.state == .suspended })?.resume()
             }
         }
     }
 
 
+    final func forceResume() {
+        urlSession.getTasksWithCompletionHandler { _, _, downloadTasks in
+            OperationQueue.main.addOperation {
+                downloadTasks.first(where: { $0.state == .suspended })?.resume()
+            }
+        }
+    }
+
+
+    private override init() {
+        super.init()
+
+//        currentFileIndex = UserDefaults.standard.integer(forKey: "Bomzh")
+//        os_log("------ init(): currentFileIndex: %{public}d", currentFileIndex)
+    }
+
+
+    private(set) lazy var urlSession: URLSession = {
+        let config = URLSessionConfiguration.background(withIdentifier: "com.bomzh.uu")
+
+        return URLSession(configuration: config, delegate: self, delegateQueue: nil)
+    }()
+
+    private let filesToDownload = [
+        //        "https://devstreaming-cdn.apple.com/videos/wwdc/2017/250lnw83hnjfutowrg/250/250_sd_extend_your_apps_presence_with_deep_linking.mp4?dl=1",
+        "https://devstreaming-cdn.apple.com/videos/tutorials/ensuring_beautiful_rich_links/ensuring_beautiful_rich_links_sd.mp4?dl=1",
+        "https://devstreaming-cdn.apple.com/videos/tutorials/web_inspector_walkthrough/web_inspector_walkthrough_sd.mp4?dl=1",
+        "https://devstreaming-cdn.apple.com/videos/tutorials/using_web_inspector_with_tvos_apps/using_web_inspector_with_tvos_apps_sd.mp4?dl=1" // ,
+        //        "https://devstreaming-cdn.apple.com/videos/tutorials/20170912/202uhvrcg65c7/updating_your_app_for_apple_tv_4k/updating_your_app_for_apple_tv_4k_sd.mp4?dl=1"
+    ]
+
+    private weak var overallProgress: Progress?
+    private var currentFileIndex = 0
+    private var backgroundTaskIdentifier = UIBackgroundTaskIdentifier.invalid
+}
+
+
+
+extension BigFileDownloadManager: URLSessionDownloadDelegate {
+
+    func urlSessionDidFinishEvents(forBackgroundURLSession session: URLSession) {
+        DispatchQueue.main.async {
+            let timeRemaining = UIApplication.shared.backgroundTimeRemaining
+            os_log("------ urlSessionDidFinishEvents: backgroundTimeRemaining = %{public}f", timeRemaining)
+        }
+
+        DispatchQueue.main.async {
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate,
+                let backgroundCompletionHandler =
+                appDelegate.backgroundCompletionHandler else {
+                    return
+            }
+            backgroundCompletionHandler()
+        }
+    }
+
+
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        DispatchQueue.main.async {
+            let timeRemaining = UIApplication.shared.backgroundTimeRemaining
+            os_log("------ didCompleteWithError: backgroundTimeRemaining = %{public}f", timeRemaining)
+        }
+
+        if let error = error {
+            os_log("------ didCompleteWithError: %{public}@", error.localizedDescription)
+
+            return
+        }
+
+        session.getTasksWithCompletionHandler { _, _, downloadTasks in
+            DispatchQueue.main.async {
+                downloadTasks.first(where: { $0.state == .suspended })?.resume()
+            }
+        }
+
+//        guard let response = task.response as? HTTPURLResponse else { return }
+//        os_log("------ didCompleteWithError: nil; response: %{public}d", response.statusCode)
+
+//        DispatchQueue.main.async { [weak self] in
+//            guard let strongSelf = self else { return }
+//        backgroundTaskIdentifier = UIApplication.shared.beginBackgroundTask(withName: "com.yz.download-next-file") { [weak self] in
+//            guard let strongSelf = self else { return }
+//
+//            UIApplication.shared.endBackgroundTask(strongSelf.backgroundTaskIdentifier)
+//        }
+
+//        DispatchQueue.main.async {
+//            os_log("------ Right after beginBackgroundTask: backgroundTimeRemaining: %{public}f", UIApplication.shared.backgroundTimeRemaining)
+//        }
+        
+//        let strongSelf = self
+
+//            strongSelf.currentFileIndex += 1
+//        UserDefaults.standard.set(strongSelf.currentFileIndex, forKey: "Bomzh")
+//            strongSelf.startDownload()
+//        }
+
+//        if backgroundTaskIdentifier != .invalid {
+//            UIApplication.shared.endBackgroundTask(backgroundTaskIdentifier)
+//        }
+    }
+
+
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-        os_log("didFinishDownloadingTo")
-        os_log("--- %{public}@", downloadTaskAddresses)
+        guard let documentURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            os_log("------ FATAL!!!! Unable to find .downloadsDirectory, in: .userDomainMask")
+
+            return
+        }
+
+        let docURL = documentURL.appendingPathComponent("Temp")
+
+        do {
+            try FileManager.default.createDirectory(at: docURL, withIntermediateDirectories: false, attributes: nil)
+        } catch {
+            print("------ createDirectory: %{public}@", error.localizedDescription)
+        }
+
+        do {
+//            let zzz = try FileManager.default.contentsOfDirectory(at: docURL, includingPropertiesForKeys: nil, options: [])
+//            print("\(zzz)")
+
+            let loc = docURL.appendingPathComponent("aaa\(currentFileIndex).zip")
+            os_log("------ Target filepath: %{public}@", loc.path)
+
+            try FileManager.default.moveItem(at: location, to: loc)
+            os_log("------ Successfully moved item to %{public}@", loc.path)
+        } catch {
+            os_log("------ FATAL!!! %{public}@", error.localizedDescription)
+        }
+
+        currentFileIndex += 1
+
+        DispatchQueue.main.async {
+            let timeRemaining = UIApplication.shared.backgroundTimeRemaining
+            os_log("------ didFinishDownloadingTo: backgroundTimeRemaining = %{public}f", timeRemaining)
+        }
+
+//        guard let response = downloadTask.response as? HTTPURLResponse else { return }
+
+//        os_log("------ didFinishDownloadingTo; response: %{public}d", response.statusCode)
     }
 
 
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
-        let address = String(format: "%p", downloadTask)
-        downloadTaskAddresses.insert(address)
-        os_log("--- downloadTask: %{public}@, bytesWritten: %{public}d, totalBytesExpectedToWrite: %{public}d", address, bytesWritten, totalBytesExpectedToWrite)
+        os_log("------ bytesWritten: %{public}d, totalBytesExpectedToWrite: %{public}d", bytesWritten, totalBytesExpectedToWrite)
     }
 }
